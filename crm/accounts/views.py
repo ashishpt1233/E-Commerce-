20ed37e650a13870bebaeccd03a51985a8e8d2bd
apatel-sapiens <ashish.patel@sapiens.com>
2020-06-21 12:24:08 +0530

    futher changes

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,9 +1,59 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.urls import reverse
+from django.forms import inlineformset_factory
+from django.contrib.auth.models import Group
+from .decorators import unauthenticated_user
+from .forms import Orderform, CreateUserForm, CustomerForm
 from .models import *
+from .filters import OrderFilter
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from .decorators import *
 
 
+@unauthenticated_user
+def registerPage(request):
 
+        form=CreateUserForm()
+        if request.method =='POST':
+            form=CreateUserForm(request.POST)
+            if form.is_valid():
+                user=form.save()
+                username=form.cleaned_data.get('username')
+
+                group=Group.objects.get(name='customer')
+                print(group)
+                user.groups.add(group)
+                Customer.objects.create(
+                    user=user,
+                )
+
+                messages.success(request,'Account was created for - ' + username)
+                return  redirect(reverse('login'))
+        context={'form':form}
+        return render(request,'accounts/register.html',context)
+@unauthenticated_user
+def loginPage(request):
+        if request.method=='POST':
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+            user=authenticate(request,username=username,password=password)
+            if user:
+                login(request,user)
+                return redirect('home')
+            else:
+                messages.info(request,'Username OR password is incorrect')
+        context={}
+        return render(request,'accounts/login.html',context)
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
+@admin_only
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
@@ -15,13 +65,80 @@ def home(request):
 
     context={'orders':orders,'customers':customers,'total_orders':total_orders,'orders_delivered':orders_delivered,'orders_pending':orders_pending}
     return render(request,'accounts/Dashboard.html',context)
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer',])
+def userPage(request):
+    Orders = request.user.customer.order_set.all()
+    print("order:", Orders)
+    total_orders = Orders.count()
+    orders_delivered = Orders.filter(status='Delivered').count()
+    orders_pending = Orders.filter(status='Pending').count()
+    context={'orders':Orders,'total_orders':total_orders,'orders_delivered':orders_delivered,'orders_pending':orders_pending}
+    return render(request,'accounts/user.html',context)
 
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer',])
+def accountSettings(request):
+    customer = request.user.customer
+    form = CustomerForm(instance=customer)
+
+    if request.method =='POST':
+        form = CustomerForm(request.POST, request.FILES, instance=customer)
+        if form.is_valid():
+            form.save()
+
+    context ={'form':form}
+    return render(request, 'accounts/account_settings.html',context)
+@login_required(login_url='login')
 def products(request):
     products=Product.objects.all()
     context={'products':products}
     return render(request,'accounts/products.html',context)
 
+@login_required(login_url='login')
 def customer(request,pk):
     customer=Customer.objects.get(id=pk)
-    context={'customer':customer}
+    orders=customer.order_set.all()
+    orders_count = orders.count()
+    myFilter=OrderFilter(request.GET,queryset=orders)
+    orders=myFilter.qs
+    context={'customer':customer,'orders':orders,'orders_count':orders_count,'myFilter':myFilter}
     return render(request,'accounts/customer.html',context)
+
+@login_required(login_url='login')
+def createOrder(request,pk):
+    OrderFormSet=inlineformset_factory(Customer,Order,fields=('product','status'),extra=2)
+    customer=Customer.objects.get(id=pk)
+    # form=Orderform(initial={'customer':customer})
+    formset=OrderFormSet(queryset=Order.objects.none(),instance=customer)
+    if request.method =='POST':
+        formset=OrderFormSet(request.POST,instance=customer)
+        if formset.is_valid():
+            formset.save()
+            return redirect(reverse('home'))
+    context = {"formset": formset}
+    return render(request,'accounts/order_form.html',context)
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin'])
+def updateOrder(request,pk):
+    order=Order.objects.get(pk=pk)
+    form=Orderform(instance=order)
+    if request.method=='POST':
+        form=Orderform(request.POST,instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('home'))
+    context={"form": form}
+    return render(request,'accounts/order_form.html',context)
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['admin'])
+def deleteOrder(request,pk):
+    order=Order.objects.get(pk=pk)
+    if request.method=='POST':
+        order.delete()
+        return redirect(reverse('home'))
+    context={'item': order}
+    return render(request,'accounts/delete_order.html',context)
--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
@@ -126,4 +127,5 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),]
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
 
